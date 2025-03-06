ab50208ba64a189428c5f595f0b0321d4556dc71
Alfredo Filemon Ramirez Benitez <156857475+Moon18888@users.noreply.github.com>
2025-03-06 12:42:34 -0600

    Update extra_SP.sql

--- Database queries/Stored procedures/extra_SP.sql
+++ Database queries/Stored procedures/extra_SP.sql
@@ -1,1 +1,13 @@
+actualizar el estado de una incidencia
 
+CREATE PROCEDURE sp_update_incidencia_estatus
+@id_incidencia INT,
+@id_estatus INT
+AS
+BEGIN
+    UPDATE incidencias
+    SET id_estatus = @id_estatus
+    WHERE id_incidencia = @id_incidencia;
+END;
+
+ agregar una nota a una incidencia
