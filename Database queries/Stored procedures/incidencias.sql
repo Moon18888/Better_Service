11f6b23cbfa705af325774d02fff8b03ec547468
DiegoJRB95 <dj.romero95@gmail.com>
2025-03-06 23:05:17 -0600

    Update incidencias.sql
    Actualizado*
    
    Al ingresar incidente fecha se autoinserta y asigna status 'Open'

--- Database queries/Stored procedures/incidencias.sql
+++ Database queries/Stored procedures/incidencias.sql
@@ -1,24 +1,74 @@
-/*Ingresar Incidente*/
+/*Ingresar Incidente(actualizado)*/
  
 CREATE PROCEDURE sp_RegistrarIncidente
  
-   @p_id_incidencia INT,
-   @p_fecha DATE,
-   @p_id_usuario INT,
-   @p_short_description VARCHAR(255),
-   @p_descripcion VARCHAR(1500),
-   @p_id_urgencia INT,
-   @p_id_impacto INT,
-   @p_id_prioridad INT,
-   @p_id_categoria INT,
-   @p_id_subcategoria INT,
-   @p_id_grupos INT,
-   @p_id_especialista INT
- 
+    @p_id_incidencia INT,
+    @p_id_usuario INT,
+    @p_short_description VARCHAR(255),
+    @p_descripcion VARCHAR(1500),
+    @p_id_urgencia INT,
+    @p_id_impacto INT,
+    @p_id_prioridad INT,
+    @p_id_categoria INT,
+    @p_id_subcategoria INT,
+    @p_id_grupos INT,
+    @p_id_especialista INT,
+ @p_id_estatus VARCHAR(50)
 AS
 BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
+ 
+        
+        SET @p_id_estatus = 1;
+ 
+        
+        DECLARE @p_fecha DATETIME = GETDATE();  
+ 
+        
+        IF EXISTS (SELECT 1 FROM incidencias WHERE id_incidencia = @p_id_incidencia) 
+        BEGIN
+            ROLLBACK TRANSACTION;
+            PRINT 'La incidencia con ese ID ya existe.';
+        END
+        ELSE
+        BEGIN
+            INSERT INTO incidencias (
+                id_incidencia,  
+                id_usuario, 
+                short_description, 
+                descripcion, 
+                id_urgencia, 
+                id_impacto, 
+                id_prioridad, 
+                id_categoria, 
+                id_subcategoria, 
+                id_grupos, 
+                id_especialista
+            )
+            VALUES (
+                @p_id_incidencia,
+		 @p_fecha,
+                @p_id_usuario, 
+                @p_short_description, 
+                @p_descripcion, 
+                @p_id_urgencia, 
+                @p_id_impacto, 
+                @p_id_prioridad, 
+                @p_id_categoria, 
+                @p_id_subcategoria, 
+                @p_id_grupos, 
+                @p_id_especialista,
+		 @p_id_estatus
+            );
+ 
+            
+            COMMIT TRANSACTION;
+            SELECT 'Incidencia registrada exitosamente.' AS mensaje;
+        END
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
 -- Verificar si el cliente ya existe por ID
 
 		IF EXISTS (SELECT 1 FROM incidencias WHERE id_incidencia = @p_id_incidencia) 
